7bee9b619ed600de6d44ee256fa2558de35f2d1e
m2shad0w <05zhujiahua@gmail.com>
2015-07-15 11:52:00 +0800

    blog backup 2015年 7月15日 星期三 11时51分56秒 CST

--- .build.sh
+++ .build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-DATE='date'
+DATE=`date`
 echo "build at $DATE"
 cd ~/Documents/gitdoc/blog
 git pull git@github.com:M2shad0w/m2shad0w.github.io.git blogbackup
-hexo clean
+echo "wazxde" ｜sudo -S hexo clean
 echo "wazxde" ｜sudo -S hexo d -g
 
 echo "built successfully"
