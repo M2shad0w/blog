fd58b4612ef6e579ec51d6aa0e3f560eaa2a635d
m2shad0w <05zhujiahua@gmail.com>
2015-07-15 11:47:08 +0800

    blog backup date

--- .build.sh
+++ .build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-echo "build at `date`"
+DATE='date'
+echo "build at $DATE"
 cd ~/Documents/gitdoc/blog
 git pull git@github.com:M2shad0w/m2shad0w.github.io.git blogbackup
 hexo clean
-sudo hexo d -g
-"wazxde"
+echo "wazxde" ｜sudo -S hexo d -g
+
 echo "built successfully"
 echo "git blogbackuping"
 git add *
-git commit -am "blog backup 'date'"
+git commit -am "blog backup $DATE"
 git push origin blogbackup
 echo "bolg backup successfully"
