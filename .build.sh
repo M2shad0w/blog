d3f46bb453e0cf1aeb9bd1ff56a3146182592c87
m2shad0w <05zhujiahua@gmail.com>
2015-08-29 13:12:17 +0800

    blog backup 2015年 8月29日 星期六 13时12分04秒 CST

--- .build.sh
+++ .build.sh
@@ -4,7 +4,7 @@ echo "build at $DATE"
 cd ~/Documents/gitdoc/blog
 git pull git@github.com:M2shad0w/m2shad0w.github.io.git blogbackup
 hexo clean
-sudo hexo d -g
+hexo d -g
 echo "built successfully"
 echo "git blogbackuping"
 git add *
